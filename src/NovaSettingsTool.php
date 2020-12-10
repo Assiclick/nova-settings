a144ae4382e7d1166ba6b7862c502b8978f5c6b3
Gabriele Sbaiz <gabriele.sbaiz@2601.it>
2020-12-10 15:50:24 +0100

    renderNavigation null

--- src/NovaSettingsTool.php
+++ src/NovaSettingsTool.php
@@ -8,25 +8,15 @@ use Laravel\Nova\Tool;
 class NovaSettingsTool extends Tool
 {
     protected static $settingsFields = [];
+
     protected static $customFormatter;
-
-    public function boot()
-    {
-        Nova::script('nova-settings', __DIR__.'/../dist/js/tool.js');
-        Nova::style('nova-settings', __DIR__.'/../dist/css/tool.css');
-    }
-
-    public function renderNavigation()
-    {
-        return view('nova-settings::navigation');
-    }
 
     /**
      * Define settings fields and an optional custom format function.
      *
-     * @param array $settingsFields Array of Nova fields to be displayed.
-     * @param \Closure $customFormatter A function that takes key and value as arguments, formats and returns the value.
-     **/
+     * @param array    $settingsFields  array of Nova fields to be displayed
+     * @param \Closure $customFormatter a function that takes key and value as arguments, formats and returns the value
+     */
     public static function setSettingsFields($settingsFields = [], $customFormatter = null)
     {
         self::$settingsFields = $settingsFields;
@@ -41,5 +31,18 @@ class NovaSettingsTool extends Tool
     public static function getCustomFormatter()
     {
         return self::$customFormatter;
+    }
+
+    public function boot()
+    {
+        Nova::script('nova-settings', __DIR__ . '/../dist/js/tool.js');
+        Nova::style('nova-settings', __DIR__ . '/../dist/css/tool.css');
+    }
+
+    public function renderNavigation()
+    {
+        return null;
+
+        return view('nova-settings::navigation');
     }
 }
